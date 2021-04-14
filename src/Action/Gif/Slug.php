5d075f12032a000a193ea2a0a8d55a2ce942bbd6
Vincent Tommasi <tommasi.v@gmail.com>
2021-04-14 16:45:17 +0200

    Render image instead of the page for discord/slack bots

--- src/Action/Gif/Slug.php
+++ src/Action/Gif/Slug.php
@@ -6,11 +6,18 @@ namespace KaamelottGifboard\Action\Gif;
 
 use KaamelottGifboard\Action\AbstractAction;
 use KaamelottGifboard\Exception\PouletteNotFoundException;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class Slug extends AbstractAction
 {
-    public function __invoke(string $slug): Response
+    private const SHARING_APP_USER_AGENTS = [
+        'api.slack.com/robots',
+        // 'Discordbot',
+    ];
+
+    public function __invoke(Request $request, string $slug): Response
     {
         $gifs = $this->finder->findGifsBySlug($slug);
 
@@ -18,6 +25,26 @@ class Slug extends AbstractAction
             throw new PouletteNotFoundException('slug', $slug);
         }
 
+        if ($this->isASharingApp($request)) {
+            $image = (array) parse_url($gifs['current']->image);
+
+            if (\array_key_exists('path', $image)) {
+                $response = new BinaryFileResponse(ltrim($image['path'], '/'));
+                $response->headers->set('Content-Type', 'image/gif');
+
+                return $response;
+            }
+        }
+
         return $this->render('gif.html.twig', $gifs);
+    }
+
+    private function isASharingApp(Request $request): bool
+    {
+        $userAgent = (string) $request->headers->get('User-Agent');
+
+        $userAgents = (string) implode('|', self::SHARING_APP_USER_AGENTS);
+
+        return (bool) preg_match(sprintf('#%s#', $userAgents), $userAgent);
     }
 }
