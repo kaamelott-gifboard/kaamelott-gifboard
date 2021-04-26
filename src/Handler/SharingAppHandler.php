e50df1955bc4863d12538bbc7c419035b052cac9
Vincent Tommasi <tommasi.v@gmail.com>
2021-04-26 19:33:16 +0200

    Redirect old slugs to new ones (301) + refacto sharing app handler

---
+++ src/Handler/SharingAppHandler.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KaamelottGifboard\Handler;
+
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class SharingAppHandler
+{
+    public function __construct(
+        private RequestStack $requestStack,
+        private string $publicPath,
+    ) {
+    }
+
+    private const SHARING_APP_USER_AGENTS = [
+        'api.slack.com',
+//        'Discordbot',
+    ];
+
+    public function getResponse(array $gifs): ?BinaryFileResponse
+    {
+        if (!$request = $this->requestStack->getMasterRequest()) {
+            return null;
+        }
+
+        if (!$this->isASharingApp($request)) {
+            return null;
+        }
+
+        $image = (array) parse_url($gifs['current']->image);
+
+        if (\array_key_exists('path', $image)) {
+            $file = new File($this->publicPath.$image['path']);
+
+            $response = new BinaryFileResponse($file);
+            $response->headers->set('Content-Type', 'image/gif');
+            $response->headers->set('Content-Length', (string) $file->getSize());
+
+            return $response;
+        }
+
+        return null;
+    }
+
+    private function isASharingApp(Request $request): bool
+    {
+        $userAgent = (string) $request->headers->get('User-Agent');
+
+        $userAgents = (string) implode('|', self::SHARING_APP_USER_AGENTS);
+
+        return (bool) preg_match(sprintf('#%s#', $userAgents), $userAgent);
+    }
+}
---
+++ tests/Handler/SharingAppHandlerTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KaamelottGifboard\Tests\Handler;
+
+use KaamelottGifboard\DataObject\Gif;
+use KaamelottGifboard\Handler\SharingAppHandler;
+use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class SharingAppHandlerTest extends KernelTestCase
+{
+    private MockObject | RequestStack $requestStack;
+    private SharingAppHandler $handler;
+
+    protected function setUp(): void
+    {
+        $this->requestStack = $this->createMock(RequestStack::class);
+
+        $this->handler = new SharingAppHandler(
+            $this->requestStack,
+            __DIR__.'/../../public'
+        );
+    }
+
+    public function testNoRequest(): void
+    {
+        $this->requestStack
+            ->expects(static::once())
+            ->method('getMasterRequest')
+            ->willReturn(null);
+
+        static::assertNull($this->handler->getResponse([]));
+    }
+
+    public function testIsNotASharingApp(): void
+    {
+        $request = new Request();
+        $request->headers->set('User-Agent', 'Foobar');
+
+        $this->requestStack
+            ->expects(static::once())
+            ->method('getMasterRequest')
+            ->willReturn($request);
+
+        static::assertNull($this->handler->getResponse([]));
+    }
+
+    public function testIsASharingApp(): void
+    {
+        $request = new Request();
+        $request->headers->set('User-Agent', 'api.slack.com/robots');
+
+        $this->requestStack
+            ->expects(static::once())
+            ->method('getMasterRequest')
+            ->willReturn($request);
+
+        $gif = new Gif();
+        $gif->image = 'https://kaamelott-gifboard.fr/gifs/elle-est-ou-la-poulette.gif';
+
+        $response = $this->handler->getResponse(['current' => $gif]);
+
+        static::assertInstanceOf(BinaryFileResponse::class, $response);
+        static::assertSame('image/gif', $response->headers->get('Content-Type'));
+        static::assertSame('24650', $response->headers->get('Content-Length'));
+    }
+}
