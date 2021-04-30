2936e6a79781dfc94e2cd87b5e6889dbdaf7d35d
Vincent Tommasi <tommasi.v@gmail.com>
2021-05-01 00:44:59 +0200

    Add ajax autocomplete for homepage

---
+++ src/Action/Xhr/SearchQuotes.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KaamelottGifboard\Action\Xhr;
+
+use KaamelottGifboard\Action\AbstractAction;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SearchQuotes extends AbstractAction
+{
+    public function __invoke(Request $request): Response
+    {
+        if (!$request->isXmlHttpRequest()) {
+            return new Response(null, Response::HTTP_NOT_FOUND);
+        }
+
+        $gifs = $this->finder->findGifsByQuote(
+            (string) $request->query->get('search', '')
+        );
+
+        return $this->render('includes/list.html.twig', ['gifs' => $gifs]);
+    }
+}
