1af1c20057229206f1b6112952c31f72731f92d0
Vincent Tommasi <tommasi.v@gmail.com>
2021-05-05 00:15:31 +0200

    Improve footer + add donate button

--- src/Action/Gif/Random.php
+++ src/Action/Gif/Random.php
@@ -5,14 +5,20 @@ declare(strict_types=1);
 namespace KaamelottGifboard\Action\Gif;
 
 use KaamelottGifboard\Action\AbstractAction;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class Random extends AbstractAction
 {
-    public function __invoke(): Response
+    public function __invoke(Request $request): Response
     {
         $random = $this->finder->findRandom();
+
+        if ($request->isXmlHttpRequest()) {
+            return new JsonResponse($random['current']);
+        }
 
         return new RedirectResponse($random['current']->url, Response::HTTP_SEE_OTHER);
     }
