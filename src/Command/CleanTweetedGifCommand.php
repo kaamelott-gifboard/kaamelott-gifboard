4658dcf7a05bd974f55c73761c275d35d49166a2
Vincent Tommasi <tommasi.v@gmail.com>
2021-12-10 22:17:38 +0100

    Tweet random gif not recently posted + deletion command

---
+++ src/Command/CleanTweetedGifCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KaamelottGifboard\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+
+class CleanTweetedGifCommand extends Command
+{
+    private const MAX_DAYS_BEFORE_DELETION = 14;
+
+    protected static $defaultName = 'tweet:clean:posted';
+
+    public function __construct(
+        private string $publicPath,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Clean the recently posted GIF to @KGifboard')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $deleted = 0;
+
+        $postedGifs = (new Finder())->files()->in($this->publicPath.'/twitter');
+
+        foreach ($postedGifs as $postedGif) {
+            $tweetCreatedAt = (new \DateTime())->setTimestamp($postedGif->getCTime());
+
+            // Gif posted more than two weeks ago.
+            if ($tweetCreatedAt->diff(new \DateTime())->days > self::MAX_DAYS_BEFORE_DELETION) {
+                unlink($postedGif->getPathname());
+
+                ++$deleted;
+            }
+        }
+
+        $output->writeln(sprintf('%d GIF has been deleted', $deleted));
+
+        return Command::SUCCESS;
+    }
+}
